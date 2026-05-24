4671b5e8d6a6b771203237be97b2d5d5d9a12565
travelmind92 <travelmindblow92@gmail.com>
2026-05-24 01:36:30 -0300

    Change env vars.

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -10,14 +10,10 @@ const API_BASE_URL = apiConfig.useLocalApi
 
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV ?? "development",
-  AWS_REGION: process.env.REACT_APP_AWS_REGION ?? "",
-  AWS_S3_BUCKET: process.env.REACT_APP_AWS_S3_BUCKET ?? "",
   AWS_COGNITO_DOMAIN: process.env.REACT_APP_AWS_COGNITO_DOMAIN ?? "",
   AWS_COGNITO_APP_CLIENT_ID: process.env.REACT_APP_AWS_COGNITO_APP_CLIENT_ID ?? "",
   AWS_COGNITO_REDIRECT_URI: process.env.REACT_APP_AWS_COGNITO_REDIRECT_URI ?? "",
-  API_BASE_URL,
-  AWS_API_GATEWAY_URL: API_BASE_URL,
-  USE_LOCAL_API: apiConfig.useLocalApi,
+  API_BASE_URL
 } as const;
 
 if (process.env.NODE_ENV === "development" && API_BASE_URL) {
